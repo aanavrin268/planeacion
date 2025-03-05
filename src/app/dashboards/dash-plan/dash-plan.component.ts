79789147f8688793afab6b424559a382e5e0100e
aanavrin268 <aanvavrin268@gmail.com>
2025-03-04 19:21:43 -0600

    changes

--- src/app/dashboards/dash-plan/dash-plan.component.ts
+++ src/app/dashboards/dash-plan/dash-plan.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalAddNewPlanComponent } from '../../shared/components/modals/modal-add-new-plan/modal-add-new-plan.component';
 import { ModalPlanViewComponent } from '../../shared/components/modals/modal-plan-view/modal-plan-view.component';
+import { gsap } from 'gsap';
+
+
 
 @Component({
   selector: 'app-dash-plan',
@@ -13,6 +16,9 @@ import { ModalPlanViewComponent } from '../../shared/components/modals/modal-pla
 export class DashPlanComponent implements OnInit {
 
   protected plan_list:any[] = [];
+  protected showAn: boolean = false;
+
+  @ViewChild('menu') menu!: ElementRef;
 
 
   constructor(private modal: NgbModal){
@@ -27,6 +33,43 @@ export class DashPlanComponent implements OnInit {
 
 
   }
+
+
+  openIt() {
+    if (this.showAn) {
+      // Cerrar menú con animación de salida
+      gsap.to(this.menu.nativeElement, {
+        opacity: 0,
+        scale: 0.5,
+        duration: 0.3,
+        ease: 'power2.in',
+        onComplete: () => {
+          this.showAn = false;
+          this.menu.nativeElement.style.display = "none"; // Ocultamos después de la animación
+        }
+      });
+    } else {
+      this.showAn = true; // Activamos la bandera primero
+  
+      // Aseguramos que el menú esté visible antes de animar
+      this.menu.nativeElement.style.display = "block";
+  
+      // Definimos manualmente el estado inicial antes de la animación
+      gsap.set(this.menu.nativeElement, { opacity: 0, scale: 0.5 });
+  
+      // Animación de entrada
+      gsap.to(this.menu.nativeElement, {
+        opacity: 1,
+        scale: 1,
+        duration: 0.5,
+        ease: 'power2.out'
+      });
+    }
+  }
+  
+  
+
+
 
   addNewPlan(){
     const modalRef = this.modal.open(ModalAddNewPlanComponent, 
