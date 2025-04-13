3338243d2e7ae7bc16bc5a33263228e49fead383
aanavrin268 <aanvavrin268@gmail.com>
2025-04-13 15:47:57 -0600

    shared folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { QuatriComponent } from './quatri/quatri.component';
-import { TestComponent } from './test/test.component';
 import { FichaTecnicaComponent } from './producto/ficha-tecnica/ficha-tecnica.component';
 import { DraggableComponent } from './producto/draggable/draggable.component';
 import { CanicasComponent } from './canicas/canicas.component';
 import { DashComponent } from './producto/dash/dash.component';
 import { ProductComponent } from './producto/product/product.component';
-import { RepFincadasComponent } from './reportes/rep-fincadas/rep-fincadas.component';
-import { UFincadasComponent } from './shared/semi/u-fincadas/u-fincadas.component';
 import { HomeComponent } from './dashboards/home/home.component';
 import { DashProductsComponent } from './dashboards/dash-products/dash-products.component';
 import { DashReportsComponent } from './dashboards/dash-reports/dash-reports.component';
@@ -29,8 +26,6 @@ import { MainComparativaComponent } from './plan/main-comparativa/main-comparati
 export const routes: Routes = [
 {path:  'quatri', component: QuatriComponent},
 {path: 'product', component: ProductComponent},
-{path: 'repFincadas', component: RepFincadasComponent},
-{path: 'uFincadas', component: UFincadasComponent},
 {path: 'home', component: HomeComponent},
 {path: 'dashProducts', component: DashProductsComponent},
 {path: 'dashReports', component: DashReportsComponent},
@@ -73,7 +68,6 @@ export const routes: Routes = [
 
 
 
-{path: 'test', component:TestComponent},
 {path: '', component: MainComparativaComponent, pathMatch: 'full'}
 
 
